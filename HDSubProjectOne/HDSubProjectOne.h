9c5c4219768c080ec60ae0e5d94d335397323af2
Harry <328418417@qq.com>
2016-04-27 09:57:48 +0800

    初始化子项目1

---
+++ HDSubProjectOne/HDSubProjectOne.h
@@ -0,0 +1,19 @@
+//
+//  HDSubProjectOne.h
+//  HDSubProjectOne
+//
+//  Created by Harry on 16/4/27.
+//  Copyright © 2016年 HarryDeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HDSubProjectOne.
+FOUNDATION_EXPORT double HDSubProjectOneVersionNumber;
+
+//! Project version string for HDSubProjectOne.
+FOUNDATION_EXPORT const unsigned char HDSubProjectOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HDSubProjectOne/PublicHeader.h>
+
+
